354c211b5b5dc12d1e6266f990dcb029c779488b
yelim237 <yelim2377@gmail.com>
2020-12-01 17:33:11 -0500

    my new version

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' myData
+#'
+#' `myData` is a data of 100 students who took a general chemistry course at Virginia Tech in Fall 2020.
+#'
+#' @format A data frame with 100 rows and 6 variables:
+#' \describe{
+#'   \item{Y}{this is an overall score in the General Chemistry in Fall 2020.}
+#'   \item{X1}{this is time spent on studying.}
+#'   \item{X2}{this is number of game a student playing.}
+#'   \item{X3}{this is number of friends a student has.}
+#'   \item{X4}{this is time spent on commute.}
+#'   \item{X5}{this is time spent on shopping per week.}
+#' }
+#' @source
+"myData"
